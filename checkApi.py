79cd5ab0efdbee10e042dd82251576f853b5577f
Sainath <sainathreddynarahari6@gmail.com>
2020-05-12 22:24:43 -0400

    Update

--- checkApi.py
+++ checkApi.py
@@ -5,16 +5,37 @@ colId = [1, 2, 3]
 
 url = "https://jsonplaceholder.typicode.com/posts/"
 
-for id in colId:
-    try:
-        getById = url+str(id)
-        r = requests.get(getById)
-        if(r.status_code == 200):
-            result = r.json()
-            print("ID With str converter :  " + str(result["id"]))
-            print(f"ID With format :  {result['id']}")
-            print("Title :  " + result["title"])
-            print("Body :  " + result["body"])
-            print("\n")
-    except Exception as err:
-        print(f'Other error occurred: {err}')
+
+def getById(args):
+    for id in colId:
+        try:
+            getById = url+str(id)
+            r = requests.get(getById)
+            if(r.status_code == 200):
+                result = r.json()
+                printValues(result)
+        except Exception as err:
+            print(f'Other error occurred: {err}')
+
+
+def getAllValues():
+    for id in colId:
+        try:
+            r = requests.get(url)
+            if(r.status_code == 200):
+                result = r.json()
+                for res in result:
+                    printValues(res)
+        except Exception as err:
+            print(f'Other error occurred: {err}')
+
+
+def printValues(result):
+    print("ID With str converter :  " + str(result["id"]))
+    print(f"ID With format :  {result['id']}")
+    print("Title :  " + result["title"])
+    print("Body :  " + result["body"])
+    print("\n")
+
+
+getAllValues()
